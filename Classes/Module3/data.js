a6e58fbdcf9bd1e1e95e0701f5dd2fe1f495fea3
Arnxld <pedro.arnold13@gmail.com>
2020-05-27 16:15:07 -0300

    module 3

---
+++ Classes/Module3/data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS animation | Mayk Brito",
+        duration: "57min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API? REST E RESTFUL? | Mayk Brito",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "u99tNt3TZf8",
+        title: "Desvendando o Redux na prática",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "vqrjFnq3-uo",
+        title: "Criando player de áudio | Mayk Brito",
+        duration: "57min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: High Order Functions | Mayk Brito",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git e Github para iniciantes: na prática | Mayk Brito",
+        duration: "57min",
+        price: "Free",
+        featured: true
+    }
+]
