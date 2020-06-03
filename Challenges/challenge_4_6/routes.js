83e24bcd842106d44e539015dea56c4384135dc3
Arnxld <pedro.arnold13@gmail.com>
2020-06-03 00:49:39 -0300

    challenge 4-6 completed

---
+++ Challenges/challenge_4_6/routes.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const routes = express.Router()
+const teachers = require('./teachers')
+
+routes.get('/', function(req, res) {
+    res.redirect('/teachers')
+})
+
+routes.get('/teachers', teachers.index)
+
+routes.get('/teachers/create', function(req, res) {
+    res.render('teachers/create')
+})
+
+routes.post('/teachers', teachers.post)
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
+
+routes.put('/teachers', teachers.put)
+
+routes.delete('/teachers', teachers.delete)
+
+
+routes.get('/students', function(req,res) {
+    res.render("students/index")
+})
+
+module.exports = routes
