e75891420bd82992993357920d789b8983b746d5
Arnxld <pedro.arnold13@gmail.com>
2020-06-12 19:24:41 -0300

    README updated

---
+++ Challenges/challenge_5_1/src/app/controllers/teachers.js
@@ -0,0 +1,42 @@
+const Intl = require('intl')
+const {age, degree, date, grade} = require('../../lib/utils')
+
+
+module.exports = {
+    index(req,res) {
+        res.render("teachers/index")
+    },
+    create(req,res) {
+        res.render('teachers/create')
+    },
+    post(req,res) {
+        const keys = Object.keys(req.body)
+
+        for(key of keys) {
+            if (req.body[key] == "") return res.send('please fill all files')
+        }
+
+        return
+    },
+    show(req,res) {
+        return
+    },
+    edit(req,res) {
+        
+
+        return
+    },
+    put(req,res) {
+        const keys = Object.keys(req.body)
+
+        for(key of keys) {
+            if (req.body[key] == "") return res.send('please fill all files')
+        }
+
+        return
+    },
+    delete(req,res) {
+        return
+    }
+ 
+}
