d8231067c4f9f24e88fc19dd5c2b956aecfaac26
Arnxld <pedro.arnold13@gmail.com>
2020-07-28 08:43:08 -0300

    module 8 finished with search sql

--- Classes/module6/src/app/controllers/SearchController.js
+++ Classes/module6/src/app/controllers/SearchController.js
@@ -6,27 +6,58 @@ const File = require('../models/File')
 
 module.exports = {
     async index(req,res) {
-        let results = await Product.all()
-        const products = results.rows
-        if(!products) return res.send("Products not found!")
+        try {
+            let results,
+                params = {}
 
+            const {filter, category} = req.query
 
-        async function getImage(productId) {
-            let results = await Product.files(productId)
-            const files = results.rows.map(file =>  `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+            if(!filter)     return res.redirect("/")
 
-            return files[0]
+            params.filter = filter 
+
+            if(category) {
+                params.category = category
+            }
+
+            results = await Product.search(params)
+
+            async function getImage(productId) {
+                let results = await Product.files(productId)
+                const files = results.rows.map(file =>  `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+    
+                return files[0]
+            }
+
+            const ProductsPromise = results.rows.map(async product => {
+                product.img = await getImage(product.id)
+                product.oldPrice = formatPrice(product.old_price)
+                product.price = formatPrice(product.price)
+                return product
+            })
+
+            const products = await Promise.all(ProductsPromise)
+
+            const search = {
+                term: req.query.filter,
+                total: products.length
+            }
+
+            const categories = products.map(product => ({
+                id: product.catetogry_id,
+                name: product.category_name
+            })).reduce((categoriesFiltered, category) => {
+
+                const found = categoriesFiltered.some(cat => cat.id == category.id) // entra vazio
+                
+                if(!found) categoriesFiltered.push(category)
+                return categoriesFiltered
+            }, []) // [{id, name}]
+
+            return res.render("search/index", {products, search, categories})
+
+        } catch(err) {
+            console.error(err)
         }
-
-        const productsPromise = products.map(async product => {
-            product.img = await getImage(product.id)
-            product.oldPrice = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-            return product
-        }).filter((product, index) => index > 2? false : true)
-
-        const lastAdded = await Promise.all(productsPromise)
-
-        return res.render("search/index", {products: lastAdded})
-    }
+    }   
 }
--- Classes/module6/src/app/models/Product.js
+++ Classes/module6/src/app/models/Product.js
@@ -75,5 +75,48 @@ module.exports = {
 
     files(id) {
         return db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
+    },
+    search(params) {
+        const {filter, category} = params
+        let query = "",
+            filterQuery = `WHERE`
+
+        if(category) {
+            filterQuery = `${filterQuery}
+            products.category_id = ${category}
+            AND`
+        }
+
+        filterQuery = `
+            ${filterQuery}
+            products.name ILIKE '%${filter}%'
+            OR products.description ILIKE '%${filter}%'
+        `
+
+        // total alternativo
+
+        // let total_query = `(
+        //     SELECT count(*) FROM products
+        //     ${filterQuery}
+        // ) AS total`
+
+        // query = `
+        //     SELECT products.*, ${total_query},
+        //         categories.name AS category_name
+        //     FROM products
+        //     LEFT JOIN categories ON (categories.id = products.categoriy_id)
+        //     ${filterQuery}
+        //     GROUP BY products.id, categories.name
+        // `
+
+        query =  `
+        SELECT products.*,
+            categories.name AS category_name
+            FROM products
+        LEFT JOIN categories ON (categories.id = products.category_id)
+        ${filterQuery}
+        `
+
+        return db.query(query)
     }
 }
